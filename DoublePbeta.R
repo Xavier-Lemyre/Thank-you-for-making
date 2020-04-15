21e0c67ed15cfc93fdad632a92761034d4e72ca1
Xavier Lemyre <63497421+Xavier-Lemyre@users.noreply.github.com>
2020-04-15 03:21:52 -0400

    Add files via upload
    It holds a lead, learns alone, blossoms and takes its tim

---
+++ DoublePbeta.R
@@ -0,0 +1,41 @@
+is.R()#? 
+#utf8
+#Making from scratch:
+rm(list = ls())
+# MACHINE LEARNING WITH R
+#<br>
+#Xavier Lemyre
+#<br>
+#MITx
+#<br>
+#April 12-13, 2020
+
+#<br><br>
+
+# ACKNOWLEDGEMENTS
+
+#<br>
+
+#In grateful recognition of MIT's support to open-source, Thank you.
+
+#<br>
+
+#Copyright © 2020
+#<br>
+#Open ◎ 2020 X
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+#<br><br>
+
+## R
+
+#```{r, echo = TRUE}
+#is.R()#? #utf8
+#```
+
+#```{r, echo = FALSE}
+#cat("THE SOFTWARE IS PROVIDED `AS IS`, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.")
+#```
